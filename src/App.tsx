da3ba4df800b63ac70ceaa527ed2c9449e4b0737
Syleiman <raftti279@gmail.com>
2024-09-01 02:04:52 +0300

    rofl time

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
 
   return (
     <>
+        {/*
       <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
+         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
