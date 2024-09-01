7328b213b1d6d75a83740ee6863c23569815ba91
Syleiman <raftti279@gmail.com>
2024-09-01 11:38:04 +0300

    fofl time

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Page from "./components/layouts/Page";
 function App() {
   return (
     <Page title="How are you?)">
-      <p>а мне нравится жопы</p>
+      <p>кто прочитал тот лох</p>
     </Page>
   );
 }
