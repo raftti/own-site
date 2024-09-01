becff9b51f0e7acadfc1f03514b3610c9e6d2587
Syleiman <raftti279@gmail.com>
2024-09-01 03:22:26 +0300

    feat: created a layout and changed favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import './App.css'
+import "./App.css";
+import Page from "./components/layouts/Page";
 
 function App() {
-
   return (
-    <>
-        {/*
-      <div>
-         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      мне нравится жопа
-    </>
-  )
+    <Page title="How are you?)">
+      <p>а мне нравится жопы</p>
+    </Page>
+  );
 }
 
-export default App
+export default App;
